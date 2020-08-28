326d5275cb202bfba02b5b2f604bd144e19ac22f
Leandro Arista <45300662+LeandroArista@users.noreply.github.com>
2020-08-28 15:52:04 -0300

    resolcucion inciso b filas pares e impares
    pares maximo
    impares minimo

---
+++ TP1/script/script.js
@@ -0,0 +1,61 @@
+let col=10;
+let row=10;
+
+let matriz = [];
+
+for (let i = 0; i < row ;i++){
+    matriz[i]=[];
+    for (let j=0; j < col;j++){
+        let num=Math.floor(Math.random()*100);
+        matriz[i][j]=num;
+        FileReader.innerHTMl+="<td>"+num+"</td>";
+    }
+}
+
+console.table(matriz);
+
+incisoA(matriz)
+//inciso a Escribir una función que retorne el valor máximo de toda la matriz 
+function incisoA(){
+    let max = 0;
+    for (let i=0;i< row;i++){
+        for (let j=0;j< row ;j++){
+            if (matriz[i][j] > max){
+                max=matriz[i][j];
+            }
+        }
+    }
+    console.log("valor maximo tabla "+max);
+}
+//inciso b Escribir una función que retorne el valor máximo contenido en las filas pares y el valor mínimo en las filas impare
+function incisoB(){
+    let max = 0;
+    let min = 9999;
+    for (let i=0;i< row;i++){
+        max = 0;
+        min = 9999;
+        for (let j=0;j< row ;j++){
+            if ( i % 2 == 0 ){
+               if (matriz[i][j]>max){
+                   max = matriz[i][j];
+               }
+            }else{
+                if (matriz[i][j]<min){
+                    min = matriz[i][j];
+                }
+            }
+        }
+        let fila=i;
+        if ( i % 2 == 0){
+            FileReader.innerHTMl+="<p> valor maximo fila "+fila+" "+max+"</p>";
+            console.log("valor maximo fila "+fila+" "+max);
+        } 
+        else{   
+            FileReader.innerHTMl+="<p>valor minimo fila "+fila+" "+min+"</p>";
+            console.log("valor minimo fila "+fila+" "+min);
+        }
+    }
+}
+incisoB(matriz);
+
+
