9e74afe44e7fda7a8cde75a374eac9c3c5f78546
Leandro Arista <45300662+LeandroArista@users.noreply.github.com>
2020-08-28 16:48:37 -0300

    cambio colores inciso 2

--- TP1/EJ2/script/script.js
+++ TP1/EJ2/script/script.js
@@ -3,7 +3,7 @@
 //Pintar una región rectangular de un color utilizando el Contexto de HTML5. 
 let ctx = document.querySelector("#canvas").getContext("2d");
 ctx.fillStyle = "#000000";
-ctx.fillRect(250,25,150,100);
+ctx.fillRect(200,150,400,280);
 ctx.stroke();
 
 
