1e61fc51754f4aa525ede0b3796597abc42168bb
Leandro Arista <45300662+LeandroArista@users.noreply.github.com>
2020-09-11 17:58:18 -0300

    arreglo visual , agrego funcion goma, filtros
    falta revisra como pinta el pincel ,redimension de imagenes , y podria agregar una imagen oculta para mantener calidad de la imagen

--- TP1/Paint/script/filtros.js
+++ TP1/Paint/script/filtros.js
@@ -1,15 +1,5 @@
-<<<<<<< Updated upstream
-let canvas = document.querySelector('#canvas');
-let context = canvas.getContext("2d");
-
-let file = e.target.files[0];
-
-let reader = new FileReader();
-reader.readAsDataURL(file);
-=======
 //let canvas = document.querySelector('#canvas');
 //let context = canvas.getContext("2d");
->>>>>>> Stashed changes
 
 function getR(imageData , x , y){
     index = (x + y * imageData.width)*4;
@@ -23,32 +13,9 @@ function getB(imageData , x , y){
     index = (x + y * imageData.width)*4;
     return imageData.data[index+2];
 }
-<<<<<<< Updated upstream
-reader.onload = readerEvent => {
-    let content = readerEvent.target.result;
-    let image = new Image();
-
-    context.drawImage(this,0,0,imageScaledWidth, imageScaledHeight);
-
-    let imageData = context.getImageDAta(0,0,imageScaledWidth,imageScaledHeight);
-
-    //filtros
-}
-
-
-//filtros grises
-function filtroGrises(){
-    for( let j = 0; j < imageData.height; j++){
-        for ( let i = 0; i < imageData.width; i++){
-=======
 
     
 function aplicarFiltro(filtro){
-    /* let reader = new FileReader();
-    reader.readAsDataURL(canvas.toDataURL("image/png"));
-  
-    reader.onload = readerEvent =>{
-        let content =readerEvent.target.result; */
         let image = new Image();
         image.src = canvas.toDataURL("image/png");
         image.onload= function (){
@@ -63,126 +30,102 @@ function aplicarFiltro(filtro){
                     filtroBN(imageData);
                 }
                 break;
-                case 'valor2':{
-
+                case 'sepia':{
+                    filtroSepia(imageData);
                 }
                 break;
-                
+                case 'invertir':{
+                    filtroInvertir(imageData);
+                }
+                break;
+                case 'brillo':{
+                    filtroBrillo(imageData);
+                }
+                break;
                 default:
 
                 break;
               }
         }
-    //}
-    
+}
+
+function setPixel(imageData, x, y, r, g, b, a){
+    let index = (x + y * imageData.width) * 4;
+    imageData.data[index + 0]= r;
+    imageData.data[index + 1]= g;
+    imageData.data[index + 2]= b;
+    imageData.data[index + 3]= a;
 }
 
 //filtro blanco y negro
 function filtroBN(imageData){
     for( let y = 0; y < imageData.height; y++){
         for ( let x = 0; x < imageData.width; x++){
->>>>>>> Stashed changes
             let r=getR(imageData,x,y);
             let g=getG(imageData,x,y);
             let b=getB(imageData,x,y);
             
             let grey=(r+g+b)/3;
-            
-            imageData[x,y].r=grey;
-            imageData[x,y].g=grey;
-            imageData[x,y].b=grey;
+            setPixel(imageData,x,y,grey,grey,grey,255); 
         }
-<<<<<<< Updated upstream
-        context.putImageData(imageData,0,0);
-=======
-        ctx.putImageData(imageData,0,0);
->>>>>>> Stashed changes
     }
+    ctx.putImageData(imageData,0,0);
 }
 
 //filtros Invertir
-function filtroInvertir(){
-<<<<<<< Updated upstream
-    for( let j = 0; j < imageData.height; j++){
-        for ( let i = 0; i < imageData.width; i++){
-=======
+function filtroInvertir(imageData){
     for( let y = 0; y < imageData.height; y++){
         for ( let x = 0; x < imageData.width; x++){
->>>>>>> Stashed changes
             let r=getR(imageData,x,y);
             let g=getG(imageData,x,y);
             let b=getB(imageData,x,y);
             
-            imageData[x,y].r= 255 - r;
-            imageData[x,y].g= 255 - g;
-            imageData[x,y].b= 255 - b;
+            setPixel(imageData,x,y,r,g,b,255); 
         }
-<<<<<<< Updated upstream
-        context.putImageData(imageData,0,0);
-=======
-        ctx.putImageData(imageData,0,0);
->>>>>>> Stashed changes
     }
+    ctx.putImageData(imageData,0,0);
 }
 //filtros Sepia
-function filtroSepia(){
-<<<<<<< Updated upstream
-    for( let j = 0; j < imageData.height; j++){
-        for ( let i = 0; i < imageData.width; i++){
-=======
+function filtroSepia(imageData){
     for( let y = 0; y < imageData.height; y++){
         for ( let x = 0; x < imageData.width; x++){
->>>>>>> Stashed changes
             let r=getR(imageData,x,y);
             let g=getG(imageData,x,y);
             let b=getB(imageData,x,y);
             
-            imageData[x,y].r= 255 - r;
-            imageData[x,y].g= 255 - g;
-            imageData[x,y].b= 255 - b;
+            r = 255 - r;
+            g = 255 - g;
+            b = 255 - b;
 
-            imageData[x,y].r= (r * .393)+(g * .769)+(b * .189);
-            imageData[x,y].g= (r * .349)+(g * .686)+(b * .168);
-            imageData[x,y].b= (r * .272)+(g * .534)+(b * .131);
-            
+            r = (r * .393)+(g * .769)+(b * .189);
+            g = (r * .349)+(g * .686)+(b * .168);
+            b = (r * .272)+(g * .534)+(b * .131);
+                   
+            setPixel(imageData,x,y,r,g,b,255); 
         }
-
-<<<<<<< Updated upstream
-        context.putImageData(imageData,0,0);
-=======
-        ctx.putImageData(imageData,0,0);
->>>>>>> Stashed changes
     }
+    ctx.putImageData(imageData,0,0);
 }
 ///filtro brillo
-function filtroBrillo(contraste){
+function filtroBrillo(imageData,contraste){
     if (contraste == undefined)
         contraste=100; 
     let factor = (259 * (contraste + 255))/(255 * (259 - contraste));
 
-<<<<<<< Updated upstream
-    for( let j = 0; j < imageData.height; j++){
-        for ( let i = 0; i < imageData.width; i++){
-=======
     for( let y = 0; y < imageData.height; y++){
         for ( let x = 0; x < imageData.width; x++){
->>>>>>> Stashed changes
             let r=getR(imageData,x,y);
             let g=getG(imageData,x,y);
             let b=getB(imageData,x,y);
             
-            imageData[x,y].r= factor * (r - 128) + 128;
-            imageData[x,y].g= factor * (g - 128) + 128;
-            imageData[x,y].b= factor * (b - 128) + 128;
+            r = factor * (r - 128) + 128;
+            g = factor * (g - 128) + 128;
+            b = factor * (b - 128) + 128;
             
+            setPixel(imageData,x,y,r,g,b,255); 
         }
-
-<<<<<<< Updated upstream
-        context.putImageData(imageData,0,0);
-=======
-        ctx.putImageData(imageData,0,0);
->>>>>>> Stashed changes
     }
+    ctx.putImageData(imageData,0,0);
 }
 
 
--- TP1/Paint/script/script.js
+++ TP1/Paint/script/script.js
@@ -1,79 +1,182 @@
 let canvas = document.querySelector('#canvas');
+let ctx = canvas.getContext('2d');
+
+
+let input = document.querySelector('#carga-imagen');
+
 let pintarLinea = false;
-let lineas = [];
+let tipoLinea = 'round';
+let color = '#000';
+let ancho = 5;
+  
+window.onmousemove = function (){
+    let x = window.event.clientX;
+    let y = window.event.clientY;
+}
+function getMousePos(canvas, evt) {
+    let rect = canvas.getBoundingClientRect();
+    return {
+          x: evt.clientX - rect.left,
+          y: evt.clientY - rect.top
+    };
+  }
 
-
-function lapiz(){
-    let ctx = canvas.getContext('2d');
-    let correccionX = 0;
-    let correccionY = 0;
-    let posicion = canvas.getBoundingClientRect()
-    correccionX = posicion.x;
-    correccionY = posicion.y;
-    canvas.width = 800;
-    canvas.height = 600;
-    
-    let tipoLinea = 'round';
-    let color = '#fff';
-    let ancho = 10;
-
-    canvas.addEventListener('mousedown', function(){
-        pintarLinea = true;
-        lineas.push([]);
-    }, false);
-    canvas.addEventListener('mousemove', function(event){  
-        event.preventDefault();
-        if (pintarLinea) {
-            //let ctx = canvas.getContext('2d');
-            ctx.lineJoin = ctx.lineCap = tipoLinea;
-            ctx.lineWidth = ancho;
-            ctx.strokeStyle = color;
-            let nuevaPosicionX = 0;
-            let nuevaPosicionY = 0;
-            if (event.changedTouches == undefined) {
-                nuevaPosicionX = event.layerX;
-                nuevaPosicionY = event.layerY;
-            } 
-            lineas[lineas.length - 1].push({x: nuevaPosicionX, y: nuevaPosicionY});
-            ctx.beginPath();
-            lineas.forEach(function (segmento) {
-                ctx.moveTo(segmento[0].x, segmento[0].y);
-                segmento.forEach(function (punto, index) {
-                    ctx.lineTo(punto.x, punto.y);
-                });
-            });
-            ctx.stroke();
-        }}, false);
-    canvas.addEventListener('mouseup', function(){
-        pintarLinea = false;
-    }, false);
-        
+function Borrar(){
+    tipoLinea = 'square';
+    color = '#fff';
+    ancho = 5;
+    document.addEventListener('mousedown',pulsaRaton,false);
+	document.addEventListener('mousemove',mueveRaton,false);
+	document.addEventListener('mouseup',levantaRaton,false);
 }
 
+function Pintar(){
+    tipoLinea = 'round';
+    color = '#000';
+    ancho = 5;
+	document.addEventListener('mousedown',pulsaRaton,false);
+	document.addEventListener('mousemove',mueveRaton,false);
+	document.addEventListener('mouseup',levantaRaton,false);
+}
+
+function pulsaRaton(capturo){
+    pintarLinea = true;
+    let mousePos = getMousePos(canvas, capturo);
+	ctx.beginPath();
+	ctx.moveTo(mousePos.x,mousePos.y);
+}
+
+function mueveRaton(capturo){
+    let mousePos = getMousePos(canvas, capturo);
+	if(pintarLinea){
+        ctx.lineJoin = ctx.lineCap = tipoLinea;
+        ctx.strokeStyle = color;
+        ctx.lineWidth = ancho;
+        let message = 'pintar position: ' + mousePos.x + ',' + mousePos.y;
+        console.log(message);
+		ctx.lineTo(mousePos.x,mousePos.y);
+		ctx.stroke();
+	}
+}
+
+function levantaRaton(capturo){
+	ctx.closePath();
+	pintarLinea = false;
+}
 
 function borrarCanvas(){
-    let ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function cargarImagen(){
-    let ctx = canvas.getContext('2d');
-    let image1= new Image();
-    let archivo = document.querySelector("#carga-imagen").files[0];
-    let reader = new FileReader();
-    if (archivo) {
-          reader.readAsDataURL(archivo );
-          reader.onloadend = function () {
-            image1.src= reader.result;
-            image1.onload = function (){ 
-                ctx.drawImage(image1,0,0);
-            }
-          }
-        }
-}
+input.onchange = e =>{
+    borrarCanvas();
+ 
+    let file = e.target.files[0];
 
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+  
+    reader.onload = readerEvent =>{
+        let content =readerEvent.target.result;
+        let image = new Image();
+        image.src = content;
+        image.onload= function (){
+            let imageAspectRatio = (1.0 * this.height)/this.width;
+            let imagescaledwidth = canvas.width;
+            let imagescaledheight = canvas.width * imageAspectRatio;
+
+            ctx.drawImage(this,0,0,imagescaledwidth,imagescaledheight);
+        
+        }
+    }
+    
+}
 
 function descargarImagen (el) {
     var imageURI = canvas.toDataURL("image/jpg");
     el.href = imageURI;
 };
+
+
+//nuevo 
+/* let cw = canvas.width = 600;
+let ch = canvas.height = 800;
+
+
+		if (canvas && canvas.getContext) {
+		  let ctx = canvas.getContext("2d");
+		  if (ctx) {
+		   
+
+		    canvas.addEventListener("mousemove", function(evt) {
+		      var mousePos = oMousePos(canvas, evt);
+		      marcarCoords(ctx, mousePos.x, mousePos.y)
+		    }, false);
+
+		    canvas.addEventListener("mouseout", function(evt) {
+		      limpiarCoords(ctx);
+		    }, false);
+		  }
+		}
+
+		function marcarCoords(output, x, y) {
+		  output.innerHTML = ("x: " + x + ", y: " + y);
+		  output.style.top = (y + 10) + "px";
+		  output.style.left = (x + 10) + "px";
+		  output.style.backgroundColor = "#FFF";
+		  output.style.border = "1px solid #d9d9d9"
+		  canvas.style.cursor = "pointer";
+		}
+
+		function limpiarCoords(output) {
+		  output.innerHTML = "";
+		  output.style.top = 0 + "px";
+		  output.style.left = 0 + "px";
+		  output.style.backgroundColor = "transparent"
+		  output.style.border = "none";
+		  canvas.style.cursor = "default";
+		}
+
+		function oMousePos(canvas, evt) {
+		  var ClientRect = canvas.getBoundingClientRect();
+		  return { //objeto
+		    x: Math.round(evt.clientX - ClientRect.left),
+		    y: Math.round(evt.clientY - ClientRect.top)
+		  }
+		}
+
+function oMousePosScaleCSS(canvas, evt) {
+  let ClientRect = canvas.getBoundingClientRect(), 
+      scaleX = canvas.width / ClientRect.width,
+      scaleY = canvas.height / ClientRect.height; 
+      return {
+      x: (evt.clientX - ClientRect.left) * scaleX, 
+      y: (evt.clientY - ClientRect.top) * scaleY 
+  }
+}
+
+let ultimo = {}
+
+canvas.addEventListener("mousedown", (e)=>{
+  m = oMousePosScaleCSS(canvas, e)
+  ctx.clearRect(0,0,cw,ch);
+  ultimo.x = m.x;
+  ultimo.y = m.y;
+});
+
+canvas.addEventListener("mouseup", (e)=>{
+    ultimo={}
+});
+
+
+canvas.addEventListener("mousemove", (e)=>{
+  if(ultimo.x){
+   m = oMousePosScaleCSS(canvas, e)
+    ctx.beginPath();
+    ctx.moveTo(ultimo.x,ultimo.y);
+    ctx.lineTo(m.x,m.y);
+    ctx.stroke();
+    ultimo.x = m.x;
+    ultimo.y = m.y;
+  }
+}) */
