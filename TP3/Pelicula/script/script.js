5cb578841240677d60c9959304551fc6bb3101c7
Leandro Arista <45300662+LeandroArista@users.noreply.github.com>
2020-10-16 15:16:39 -0300

    agrego contador

--- TP3/Pelicula/script/script.js
+++ TP3/Pelicula/script/script.js
@@ -42,9 +42,48 @@ function pararCancion(){
 	}
 	sonando=false;
 }
-document.querySelector(".Personajes").addEventListener("wheel", myFunction);
+/* document.querySelector(".Personajes").addEventListener("wheel", myFunction);
 
 function myFunction() {
 	let animacion=document.querySelector("#animacion");
 	
 }
+ */
+
+ /*Contador */
+const DATE_TARGET = new Date('11/09/2020 08:00 PM');
+
+const SPAN_DAYS = document.querySelector('span#days');
+const SPAN_HOURS = document.querySelector('span#hours');
+const SPAN_MINUTES = document.querySelector('span#minutes');
+const SPAN_SECONDS = document.querySelector('span#seconds');
+
+const MILLISECONDS_OF_A_SECOND = 1000;
+const MILLISECONDS_OF_A_MINUTE = MILLISECONDS_OF_A_SECOND * 60;
+const MILLISECONDS_OF_A_HOUR = MILLISECONDS_OF_A_MINUTE * 60;
+const MILLISECONDS_OF_A_DAY = MILLISECONDS_OF_A_HOUR * 24;
+
+function updateText(valor){
+	if(valor>=0 && valor<10){
+		valor="0"+valor;
+	}
+	return valor;
+}
+
+function updateCountdown() {
+    const NOW = new Date()
+    const DURATION = DATE_TARGET - NOW;
+    const REMAINING_DAYS = Math.floor(DURATION / MILLISECONDS_OF_A_DAY);
+    const REMAINING_HOURS = Math.floor((DURATION % MILLISECONDS_OF_A_DAY) / MILLISECONDS_OF_A_HOUR);
+    const REMAINING_MINUTES = Math.floor((DURATION % MILLISECONDS_OF_A_HOUR) / MILLISECONDS_OF_A_MINUTE);
+    const REMAINING_SECONDS = Math.floor((DURATION % MILLISECONDS_OF_A_MINUTE) / MILLISECONDS_OF_A_SECOND);
+
+    SPAN_DAYS.textContent = updateText(REMAINING_DAYS);
+    SPAN_HOURS.textContent = updateText(REMAINING_HOURS);
+    SPAN_MINUTES.textContent = updateText(REMAINING_MINUTES);
+    SPAN_SECONDS.textContent = updateText(REMAINING_SECONDS);
+}
+updateCountdown();
+setInterval(updateCountdown, MILLISECONDS_OF_A_SECOND);
+/*end contador */
+
